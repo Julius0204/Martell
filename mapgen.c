d8169bfd9413fa21be7802e3db9b457fd08db79c
denotavail <denotavail>
2021-12-30 17:24:07 +0100

    bugfix: initialize 'x' with '0'

--- mapgen.c
+++ mapgen.c
@@ -10,7 +10,7 @@ void genHeightmap() {
 	const short maxHeightDiff = 10;
 	srand(0); // set seed for rand()
 	// x = horizontal position; y = vertical position
-	for (short x, y = 0; x < mapLength; x++) {
+	for (short x=0, y=0; x < mapLength; x++) {
 		// y changes with a 50% chance:
 		if (x >= entryLength && rand()%2 == 0) {
 			// the chance of y getting higher or lower depends on the previous height:
