a774c2c629fa0bfd8376cd26ac12854f08abb019
Julius0204 <julius-philipp.engel@informatik.hs-fulda.de>
2022-01-03 21:12:57 +0100

    deletion of magic numbers

--- figure.c
+++ figure.c
@@ -19,7 +19,9 @@ nodelay(stdscr, TRUE);
 curs_set(0);
 //First, add a figure into the subwindow one
 mvwaddch(sub1,y_pos, x_pos, ACS_DIAMOND);
-
+//use constants instead of "magic numbers"
+const short space = 32;
+const int sleep_num = 100000;
 do{
 move(y_pos, x_pos);
 refresh();
@@ -32,38 +34,38 @@ switch(input){
 	
 	case ' ':
 		if(y_pos>=19){
-		mvwaddch(sub1, y_pos, x_pos,' ');
+		mvwaddch(sub1, y_pos, x_pos,space);
 		y_pos = jump(y_pos);
 		mvwaddch(sub1, y_pos, x_pos,ACS_DIAMOND);
 		wrefresh(sub1);
-		usleep(100000);
-		mvwaddch(sub1, y_pos, x_pos,' ');
+		usleep(sleep_num);
+		mvwaddch(sub1, y_pos, x_pos,space);
                 y_pos = jump(y_pos);
                 mvwaddch(sub1, y_pos, x_pos,ACS_DIAMOND);
                 wrefresh(sub1);
-                usleep(400000);
-		mvwaddch(sub1, y_pos,x_pos,' ');
+                usleep(sleep_num*4);
+		mvwaddch(sub1, y_pos,x_pos,space);
                 y_pos = move_down(y_pos);
                 mvwaddch(sub1, y_pos,x_pos,ACS_DIAMOND);
 		wrefresh(sub1);
-		usleep(100000);
-		mvwaddch(sub1, y_pos,x_pos,' ');
+		usleep(sleep_num);
+		mvwaddch(sub1, y_pos,x_pos,space);
                 y_pos = move_down(y_pos);
                 mvwaddch(sub1, y_pos,x_pos,ACS_DIAMOND);
 		wrefresh(sub1);
-		usleep(100000);
+		usleep(sleep_num);
 		}
 	break;
 	case KEY_LEFT:
 		if(x_pos>0){
-		mvwaddch(sub1, y_pos, x_pos,' ');
+		mvwaddch(sub1, y_pos, x_pos,space);
 		x_pos = move_left(x_pos);
 		mvwaddch(sub1, y_pos,x_pos,ACS_DIAMOND);
 		}
 	break;
 	case KEY_RIGHT:
 		if(x_pos < COLS-1){
-		mvwaddch(sub1, y_pos, x_pos,' ');
+		mvwaddch(sub1, y_pos, x_pos,space);
 		x_pos = move_right(x_pos);
 		mvwaddch(sub1, y_pos,x_pos,ACS_DIAMOND);
 		}
